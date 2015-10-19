6ba4a9b1ab9240192253e4073587ad041986224e
Conny Brunnkvist <cbrunnkvist@gmail.com>
2015-10-19 18:28:08 +0700

    Refactor and cleanup test

--- test/lib/paymentGateway.js
+++ test/lib/paymentGateway.js
@@ -2,38 +2,39 @@
 
 var Lab = require('lab');
 var lab = exports.lab = Lab.script();
-var Code = require('code');
+var expect = require('code').expect;
 
-var describe = lab.describe;
-var it = lab.it;
-var before = lab.before;
-var after = lab.after;
-var expect = Code.expect;
+lab.describe('payment gateway library', function() {
+  var expDate = new Date();
+  expDate.setDate(expDate.getDate() + 1);
+  var sampleRequest = {
+    payment: {
+      amount: 111,
+      currency: 'THB',
+    },
+    card: {
+      holder: 'A Full Name',
+      number: '4111 1111 1111 1111',
+      expireDate: expDate,
+      ccv: '000'
+    }
+  };
 
-describe('payment gateway library', function() {
-  function pluckIds(items) {
-    return items.map(function(p) {
-      return p.id
-    });
-  }
-
-  it('comes with built-in payment providers', function(done) {
+  var pg;
+  lab.beforeEach(function(done) {
     var PaymentGateway = require('../../lib/paymentGateway');
-    var pg = new PaymentGateway();
+    pg = new PaymentGateway();
+    done();
+  });
 
-    var providerIds = pluckIds(pg.getProviders());
+  lab.it('comes with built-in payment providers', function(done) {
+    var providerIds = Object.keys(pg.getProviders());
     expect(providerIds).to.include(['paypal']);
 
     done();
   });
 
-  it('can process single credit card payments', function(done) {
-    var PaymentGateway = require('../../lib/paymentGateway');
-    var pg = new PaymentGateway();
-
-    var expDate = new Date();
-    expDate.setDate(expDate.getDate() + 1);
-
+  lab.it('can process single credit card payments', function(done) {
     pg._providers = [{
       id: 'braintree',
       name: 'Braintree',
@@ -44,19 +45,6 @@ describe('payment gateway library', function() {
         });
       }
     }];
-
-    var sampleRequest = {
-      payment: {
-        amount: 111,
-        currency: 'THB',
-      },
-      card: {
-        holder: 'A Full Name',
-        number: '4111 1111 1111 1111',
-        expireDate: expDate,
-        ccv: '000'
-      }
-    };
 
     pg.processPayment(sampleRequest, function(err, result) {
       expect(result.txId).to.exist();
